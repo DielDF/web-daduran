cf6322172af4a40ad52176aa0959d570ddfbca8e
DielDF <diego.duran.3771@gmail.com>
2026-02-23 04:56:39 -0800

    Finalized Testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-zinc-950 text-zinc antialiased">
+      <body className="bg-zinc-950 text-zinc-100 antialiased">
         {children}
       </body>
     </html>
