7d63366bbf6b3cdec5165a44a85fc76f55f0002d
DielDF <diego.duran.3771@gmail.com>
2026-02-24 03:02:34 -0800

    Added Basic Custom Icon

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,9 +3,8 @@ export default function AboutPreview(){
         <section className="px-6 py-24">
             <div className="max-w-3xl mx-auto"></div>
             <h2 className="text-3xl font-semibold">About</h2>
-            <p className="mt-6 text-zinc-400 leading-relaxed"> Persistent and motivated upcoming avid learner transitioning into professional fields, with
-            outstanding adaptability and willingness to learn as my core skills. Skilled in database management
-            alongside software development, with hands-on experience through academic and personal projects, including this very website.
+            <p className="mt-6 text-zinc-400 leading-relaxed"> Software developer focused on building maintainable, scalable systems with strong foundations in database management and application architecture.
+                I approach development with adaptability and continuous improvement, applying what I learn on my projects, including this website.
             </p>
         </section>
     )
