a3e8ca20d6c8f8f8b3e462212d0a8dbfda45d83a
DielDF <diego.duran.3771@gmail.com>
2026-04-14 04:08:14 -0700

    Finished Detailed Capabilities Section.

--- app/components/Capabilities.tsx
+++ app/components/Capabilities.tsx
@@ -1,28 +1,29 @@
 export default function Capabilities(){
     return (
         <section className="px-6 py-24">
-            <div className="max-w-5xl mx-auto">
-                <h2 className="text-3xl font-semibold">What I Build</h2>
-                <div className="mt-12 grid gap-8 md:grid-cols-3">
-                    <div className="rounded-2xl border border-zinc-800 p-6">
+            <div className="max-w-6xl mx-auto">
+                <h2 className="text-3xl md:text-4xl font-semibold text-center">What I Build</h2>
+                <p className="mt-4 text-center text-zinc-400 max-w-2xl mx-auto"> Focused on building scalable, concise software across applications.</p>
+                <div className="mt-16 grid gap-8 md:grid-cols-3">
+                    <div className="group rounded-2xl border border-zinc-800 p-8 transition hover:border-amber-400 hover:bg-zinc-900/40">
                         <h3 className="text-xl font-medium">Websites</h3>
-                        <ul className="mt-4 space-y-2 text-zinc-400">
+                        <ul className="mt-6 space-y-3 text-zinc-400">
                             <li>Frontend (Next.js, Tailwind CSS ) </li>
                             <li>Backend (Node.js, REST APIs)</li>
                             <li>Systems (Deployment, DNS / SSL, Configuration)</li>
                         </ul>
                     </div>
-                    <div className="rounded-2xl border border-zinc-800 p-6">
+                    <div className="group rounded-2xl border border-zinc-800 p-8 transition hover:border-amber-400 hover:bg-zinc-900/40">
                         <h3 className="text-xl font-medium">Applications</h3>
-                        <ul className="mt-4 space-y-2 text-zinc-400">
+                        <ul className="mt-6 space-y-3 text-zinc-400">
                             <li>Games (Unity, Python)</li>
                             <li>Algorithms & GUIs (Java, C++) </li>
                             <li>System Administration (Linux, Windows)</li>
                         </ul>
                     </div>
-                    <div className="rounded-2xl border border-zinc-800 p-6">
+                    <div className="group rounded-2xl border border-zinc-800 p-8 transition hover:border-amber-400 hover:bg-zinc-900/40">
                         <h3 className="text-xl font-medium">Databases</h3>
-                        <ul className="mt-4 space-y-2 text-zinc-400">
+                        <ul className="mt-6 space-y-3 text-zinc-400">
                             <li>Serverless (AWS, DigitalOcean)</li>
                             <li>SQl(mySQL, Oracle)</li>
                             <li>NoSQL (MongoDB, XML/JSON)</li>
