4c4ff37ea9f03f4637009e85405ae19bae02ebdd
DielDF <diego.duran.3771@gmail.com>
2026-04-14 05:19:14 -0700

    Added Borders to finish Capabilities Section.

--- app/components/Capabilities.tsx
+++ app/components/Capabilities.tsx
@@ -1,10 +1,11 @@
 export default function Capabilities(){
     return (
-        <section className="px-6 py-24">
-            <div className="max-w-6xl mx-auto">
+        <section className="relative px-6 py-28 overflow-hidden border-t border-zinc-900">
+        
+            <div className="relative max-w-6xl mx-auto ">
                 <h2 className="text-3xl md:text-4xl font-semibold text-center">What I Build</h2>
                 <p className="mt-4 text-center text-zinc-400 max-w-2xl mx-auto"> Focused on building scalable, concise software across applications.</p>
-                <div className="mt-16 grid gap-8 md:grid-cols-3">
+                <div className="mt-20 grid gap-8 md:grid-cols-3">
                     <div className="group rounded-2xl border border-zinc-800 p-8 transition hover:border-amber-400 hover:bg-zinc-900/40">
                         <h3 className="text-xl font-medium">Websites</h3>
                         <ul className="mt-6 space-y-3 text-zinc-400">
@@ -25,7 +26,7 @@ export default function Capabilities(){
                         <h3 className="text-xl font-medium">Databases</h3>
                         <ul className="mt-6 space-y-3 text-zinc-400">
                             <li>Serverless (AWS, DigitalOcean)</li>
-                            <li>SQl(mySQL, Oracle)</li>
+                            <li>SQl (mySQL, Oracle)</li>
                             <li>NoSQL (MongoDB, XML/JSON)</li>
                         </ul>
                     </div>
